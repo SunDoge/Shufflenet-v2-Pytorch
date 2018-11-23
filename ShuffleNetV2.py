b6cece46b1ccb246884cf7f0ad9bbd306f637dcc
SunDoge <384813529@qq.com>
2018-11-23 16:44:12 +0800

    fix warning

--- ShuffleNetV2.py
+++ ShuffleNetV2.py
@@ -1,10 +1,11 @@
+from collections import OrderedDict
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
-from collections import OrderedDict
 from torch.nn import init
-import math
+
 
 def conv_bn(inp, oup, stride):
     return nn.Sequential(
@@ -21,14 +22,15 @@ def conv_1x1_bn(inp, oup):
         nn.ReLU(inplace=True)
     )
 
+
 def channel_shuffle(x, groups):
     batchsize, num_channels, height, width = x.data.size()
 
     channels_per_group = num_channels // groups
-    
+
     # reshape
-    x = x.view(batchsize, groups, 
-        channels_per_group, height, width)
+    x = x.view(batchsize, groups,
+               channels_per_group, height, width)
 
     x = torch.transpose(x, 1, 2).contiguous()
 
@@ -36,7 +38,8 @@ def channel_shuffle(x, groups):
     x = x.view(batchsize, -1, height, width)
 
     return x
-    
+
+
 class InvertedResidual(nn.Module):
     def __init__(self, inp, oup, stride, benchmodel):
         super(InvertedResidual, self).__init__()
@@ -45,23 +48,24 @@ class InvertedResidual(nn.Module):
         assert stride in [1, 2]
 
         oup_inc = oup//2
-        
+
         if self.benchmodel == 1:
             #assert inp == oup_inc
-        	self.banch2 = nn.Sequential(
+            self.banch2 = nn.Sequential(
                 # pw
                 nn.Conv2d(oup_inc, oup_inc, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup_inc),
                 nn.ReLU(inplace=True),
                 # dw
-                nn.Conv2d(oup_inc, oup_inc, 3, stride, 1, groups=oup_inc, bias=False),
+                nn.Conv2d(oup_inc, oup_inc, 3, stride,
+                          1, groups=oup_inc, bias=False),
                 nn.BatchNorm2d(oup_inc),
                 # pw-linear
                 nn.Conv2d(oup_inc, oup_inc, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup_inc),
                 nn.ReLU(inplace=True),
-            )                
-        else:                  
+            )
+        else:
             self.banch1 = nn.Sequential(
                 # dw
                 nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
@@ -70,33 +74,35 @@ class InvertedResidual(nn.Module):
                 nn.Conv2d(inp, oup_inc, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup_inc),
                 nn.ReLU(inplace=True),
-            )        
-    
+            )
+
             self.banch2 = nn.Sequential(
                 # pw
                 nn.Conv2d(inp, oup_inc, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup_inc),
                 nn.ReLU(inplace=True),
                 # dw
-                nn.Conv2d(oup_inc, oup_inc, 3, stride, 1, groups=oup_inc, bias=False),
+                nn.Conv2d(oup_inc, oup_inc, 3, stride,
+                          1, groups=oup_inc, bias=False),
                 nn.BatchNorm2d(oup_inc),
                 # pw-linear
                 nn.Conv2d(oup_inc, oup_inc, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup_inc),
                 nn.ReLU(inplace=True),
             )
-          
+
     @staticmethod
     def _concat(x, out):
         # concatenate along channel axis
-        return torch.cat((x, out), 1)        
+        return torch.cat((x, out), 1)
 
     def forward(self, x):
-        if 1==self.benchmodel:
-            x1 = x[:, :(x.shape[1]//2), :, :]
-            x2 = x[:, (x.shape[1]//2):, :, :]
+        if 1 == self.benchmodel:
+            # x1 = x[:, :(x.shape[1]//2), :, :]
+            # x2 = x[:, (x.shape[1]//2):, :, :]
+            x1, x2 = x.chunk(2, dim=1)
             out = self._concat(x1, self.banch2(x2))
-        elif 2==self.benchmodel:
+        elif 2 == self.benchmodel:
             out = self._concat(self.banch1(x), self.banch2(x))
 
         return channel_shuffle(out, 2)
@@ -105,9 +111,9 @@ class InvertedResidual(nn.Module):
 class ShuffleNetV2(nn.Module):
     def __init__(self, n_class=1000, input_size=224, width_mult=1.):
         super(ShuffleNetV2, self).__init__()
-        
+
         assert input_size % 32 == 0
-        
+
         self.stage_repeats = [4, 8, 4]
         # index 0 is invalid and should never be called.
         # only used for indexing convenience.
@@ -122,13 +128,13 @@ class ShuffleNetV2(nn.Module):
         else:
             raise ValueError(
                 """{} groups is not supported for
-                       1x1 Grouped Convolutions""".format(num_groups))
+                       1x1 Grouped Convolutions""".format(width_mult))
 
         # building first layer
         input_channel = self.stage_out_channels[1]
-        self.conv1 = conv_bn(3, input_channel, 2)    
-	self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        
+        self.conv1 = conv_bn(3, input_channel, 2)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
         self.features = []
         # building inverted residual blocks
         for idxstage in range(len(self.stage_repeats)):
@@ -136,22 +142,27 @@ class ShuffleNetV2(nn.Module):
             output_channel = self.stage_out_channels[idxstage+2]
             for i in range(numrepeat):
                 if i == 0:
-	            #inp, oup, stride, benchmodel):
-                    self.features.append(InvertedResidual(input_channel, output_channel, 2, 2))
+                    # inp, oup, stride, benchmodel):
+                    self.features.append(InvertedResidual(
+                        input_channel, output_channel, 2, 2))
                 else:
-                    self.features.append(InvertedResidual(input_channel, output_channel, 1, 1))
+                    self.features.append(InvertedResidual(
+                        input_channel, output_channel, 1, 1))
                 input_channel = output_channel
-                
-                
+
         # make it nn.Sequential
         self.features = nn.Sequential(*self.features)
 
         # building last several layers
-        self.conv_last      = conv_1x1_bn(input_channel, self.stage_out_channels[-1])
-	self.globalpool = nn.Sequential(nn.AvgPool2d(int(input_size/32)))              
-    
-	# building classifier
-	self.classifier = nn.Sequential(nn.Linear(self.stage_out_channels[-1], n_class))
+        self.conv_last = conv_1x1_bn(
+            input_channel, self.stage_out_channels[-1])
+        # self.globalpool = nn.Sequential(nn.AvgPool2d(int(input_size/32))) # Why Sequential?
+        self.globalpool = nn.AvgPool2d(int(input_size/32))
+
+        # building classifier
+        # self.classifier = nn.Sequential(
+        #     nn.Linear(self.stage_out_channels[-1], n_class))
+        self.classifier = nn.Linear(self.stage_out_channels[-1], n_class)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -163,12 +174,18 @@ class ShuffleNetV2(nn.Module):
         x = self.classifier(x)
         return x
 
-def shufflenetv2(width_mult=1.):
-    model = ShuffleNetV2(width_mult=width_mult)
+
+def shufflenetv2(n_class=1000, input_size=224, width_mult=1.):
+    model = ShuffleNetV2(
+        n_class=n_class, input_size=input_size, width_mult=width_mult)
     return model
-    
+
+
 if __name__ == "__main__":
     """Testing
     """
+    x = torch.rand(2, 3, 224, 224)
     model = ShuffleNetV2()
+    out = model(x)
     print(model)
+    print(out.shape)
